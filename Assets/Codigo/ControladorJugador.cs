ef5d4ce44cb72e12870605a95bfa85e8f3dae97f
hectorzzaa <hectorzzaa@gmail.com>
2023-02-03 18:44:13 +0100

    Initial commit

---
+++ Assets/Codigo/ControladorJugador.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorJugador : MonoBehaviour
+{
+
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey("left"))
+        {
+            //gameObject.transform.Translate(-50f * Time.deltaTime, 0, 0);
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-100 * Time.deltaTime, 0));
+
+        }
+        if (Input.GetKey("right"))
+        {
+            //gameObject.transform.Translate(50f * Time.deltaTime, 0, 0);
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(100 * Time.deltaTime, 0));
+
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            gameObject.transform.Translate(0, -15F * Time.deltaTime, 0);
+        }
+    }
+}
