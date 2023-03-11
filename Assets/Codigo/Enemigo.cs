ece6bf7268ee8c0d94fdb3cb38b3c3c73411fd0a
hectorzzaa <hectorzzaa@gmail.com>
2023-03-11 13:38:07 +0100

    Centrado todo en spawn enemigo

---
+++ Assets/Codigo/Enemigo.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+class Enemigo
+{
+    [SerializeField] private GameObject prueba;
+    private int vida;
+
+    private string tipoAtaque;
+
+    private string tipoEnemigo;
+
+    public GameObject getObjeto()
+    {
+        return prueba;
+    }
+    public void setObjeto(GameObject prueba)
+    {
+        prueba = this.prueba;
+    }
+    public int getVida()
+    {
+        return vida;
+    }
+    public void setVida(int vida)
+    {
+        vida = this.vida;
+    }
+    public string getTipoAtaque()
+    {
+        return tipoAtaque;
+    }
+    public void setTipoAtaque(string tipoAtaque)
+    {
+        tipoAtaque = this.tipoAtaque;
+    }
+
+    public string getTipoEnemigo()
+    {
+        return tipoEnemigo;
+    }
+    public void setTipoEnemigo(string tipoEnemigo)
+    {
+        tipoEnemigo = this.tipoEnemigo;
+    }
+
+    public Enemigo()
+    {
+    }
+
+    public Enemigo(int vida, string tipoAtaque, string tipoEnemigo)
+    {
+        this.vida = vida;
+        this.tipoAtaque = tipoAtaque;
+        this.tipoEnemigo = tipoEnemigo;
+    }
+
+    public Enemigo(GameObject prueba, int vida, string tipoAtaque, string tipoEnemigo)
+    {
+        this.prueba = prueba;
+        this.vida = vida;
+        this.tipoAtaque = tipoAtaque;
+        this.tipoEnemigo = tipoEnemigo;
+    }
+
+    public string decirNombre()
+    {
+        return "se ha creado al objeto" + getTipoEnemigo();
+    }
+
+
+}
+
--- Assets/Codigo/SpawnEnemigos.cs
+++ Assets/Codigo/SpawnEnemigos.cs
@@ -6,18 +6,47 @@ public class SpawnEnemigos : MonoBehaviour
 {
     [SerializeField] private GameObject enemigoPiedra;
 
-    Enemigo enePiedra = new Enemigo(1, "fisico", "piedra");
-
+    Enemigo enePiedra = new Enemigo(1,"fisico","piedra");
+    float timer;
 
 
     void Start()
     {
-        
+        enePiedra.setObjeto(enemigoPiedra);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if(enePiedra.getTipoEnemigo()=="piedra")
+        {
+
+            Vector3 a = this.transform.position;
+
+           timer += Time.deltaTime;
+            while (timer >= 3F)
+            {
+                timer = 0;
+
+                movimientoPiedra(a);
+
+            }
+        }
+
     }
+
+
+
+
+    private void movimientoPiedra(Vector3 locacion)
+    {
+        float x = Random.Range((locacion.x - 30F), (locacion.x + 30F));
+        float y = Random.Range(locacion.y, (locacion.y + 30F));
+        Vector2 position = new Vector2(x, y);
+        Quaternion rotation = new Quaternion();
+        Instantiate(enemigoPiedra, position, rotation);
+    }
+
+
 }
